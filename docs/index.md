eae1cfb43a44a9115ce52878260f70cbc1ace628
Wen Wei Tseng <sosiristseng@gmail.com>
2024-01-23 23:36:10 +0800

    Use quarto

--- docs/index.md
+++
@@ -1,3 +0,0 @@
-# Julia stuff
-
-Notes and runnable examples of Julia programming language.
--- docs/visual/index.jl
+++ docs/visual/index.jl
@@ -1,3 +1,4 @@
+md"""
 # Julia visualization example notebooks
 
 - [Summary in a discourse thread by Chris Rackauckas](https://discourse.julialang.org/t/comparison-of-plotting-packages/99860/2)
@@ -7,3 +8,12 @@
 - [Plots.jl](https://github.com/JuliaPlots/Plots.jl): powerful and convenient visualization mwith ultiple backends. See also [Plots.jl docs](http://docs.juliaplots.org/latest/)
 - [PyPlot.jl](https://github.com/JuliaPy/PyPlot.jl): `matplotlib` in Julia. See also [matplotlib docs](https://matplotlib.org/stable/index.html)
 - [Makie.jl](https://github.com/JuliaPlots/Makie.jl) see the [Makie.jl docs](https://docs.makie.org/stable/) and [beautiful Makie](https://beautiful.makie.org/).
+
+## Runtime information
+"""
+import InteractiveUtils
+InteractiveUtils.versioninfo()
+
+#---
+import Pkg
+Pkg.status()
