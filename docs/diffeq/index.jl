1211db2aac2ccc902bd7cccfdbf222bc9c3e6989
Wen Wei Tseng <sosiristseng@gmail.com>
2024-01-23 17:43:23 +0800

    Literate (#20)
    * dependabot
    
    * track dockerfile
    
    * wip
    
    * wip
    
    * rename
    
    * visual
    
    * fix plot
    
    * fix comment

---
+++ docs/diffeq/index.jl
@@ -0,0 +1,14 @@
+md"""
+# Learning Julia Differential Equations
+
+- [DifferentialEquations.jl](https://github.com/SciML/DifferentialEquations.jl): high-performance solvers of differential equations.
+
+## Runtime environment
+"""
+
+import InteractiveUtils
+InteractiveUtils.versioninfo()
+
+#---
+import Pkg
+Pkg.status()
