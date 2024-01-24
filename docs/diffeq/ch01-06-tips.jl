0822b5a886372e49ad4fe6c3dbb23a41cdb61825
Wen-Wei Tseng <sosiristseng@gmail.com>
2024-01-24 21:08:30 +0800

    NaNMath

--- docs/diffeq/ch01-06-tips.jl
+++ docs/diffeq/ch01-06-tips.jl
@@ -48,3 +48,12 @@ function smoothpulses(t, tstart, tend, period=1, amplitude=period / (tend - tsta
 end
 
 plot(t->smoothpulses(t, 0.2, 0.3, 0.5), 0.0, 2.0, lab="pulses") |> PNG
+
+#===
+## Avoiding DomainErrors
+
+Some functions like `sqrt(x)`, `log(x)`, and `pow(x)` will throw `DomainError` exceptions with negative `x`, interrupting differential equation solvers. One can use the respective functions in https://github.com/JuliaMath/NaNMath.jl, returning `NaN` instead of throwing a `DomainError`. Then, the differential equation solvers will reject the solution and retry with a smaller time step.
+===#
+
+import NaNMath as nm
+nm.sqrt(-1.0) ## returns NaN
